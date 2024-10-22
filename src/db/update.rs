51deaabba0c56a42cc88b3eadccf55053b2fad89
klover-rs <joelkerbusch@gmail.com>
2024-10-22 08:50:34 -0700

    update values in db

--- src/db/update.rs
+++ src/db/update.rs
@@ -4,6 +4,8 @@ use serde_json::Value;
 use sqlx::PgPool;
 use std::collections::HashMap;
 
+use crate::cache::{CacheKey, CACHE_MANAGER};
+
 pub struct Update;
 
 impl Update {
@@ -26,7 +28,32 @@ impl Update {
 
         println!("{:?}", query_builder);
 
-        let (_query, _params) = query_builder;
+        let (query, params) = query_builder;
+
+        let mut txn = pool.begin().await?;
+        let mut query_builder = sqlx::query::<sqlx::Postgres>(&query);
+
+        for value in params {
+            query_builder = match value {
+                Value::String(s) => query_builder.bind(s),
+                Value::Number(n) => {
+                    if let Some(num) = n.as_i64() {
+                        query_builder.bind(num)
+                    } else {
+                        return Err(anyhow::anyhow!("Invalid number type for binding"));
+                    }
+                }
+                Value::Bool(b) => query_builder.bind(b),
+                _ => return Err(anyhow::anyhow!("Unsupported value type")),
+            }
+        }
+
+        query_builder.execute(&mut *txn).await?;
+        txn.commit().await?;
+
+        let cache_key_table = CacheKey::generate_table_cache_hash(&table_name);
+
+        CACHE_MANAGER.remove_by_prefix(&cache_key_table);
 
         Ok(())
     }
